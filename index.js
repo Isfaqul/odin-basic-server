0db966e6f0bb100e278cde478440c2171116bbb0
Isfaqul <isfaq.xperia@gmail.com>
2025-09-02 11:34:19 +0530

    Add index.js again

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require("fs/promises");
+const http = require("http");
+
+// Server config
+const PORT = 8080;
+const HOST = "localhost";
+
+const server = http.createServer((req, res) => {
+  let path = "./";
+
+  if (req.url === "/") {
+    res.statusCode = 200;
+    path += "index.html";
+  } else if (req.url === "/about") {
+    res.statusCode = 200;
+    path += "about.html";
+  } else if (req.url === "/contact-me") {
+    res.statusCode = 200;
+    path += "contact-me.html";
+  } else {
+    res.statusCode = 404;
+    path += "404.html";
+  }
+
+  serveFile(path, res); // Serve the file accordingly
+});
+
+server.listen(PORT, HOST);
+
+async function serveFile(path, res) {
+  try {
+    const data = await fs.readFile(path, "utf-8");
+    res.end(data);
+  } catch (error) {
+    res.statusCode = 500;
+    console.log(error);
+    res.end();
+  }
+}
